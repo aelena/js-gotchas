dfb31a5a73551d8da4010f95eea5c92b6a10018c
aelena <antonioelena@gmail.com>
2013-05-01 00:17:07 +0200

    example07.js added

---
+++ Examples/example07.js
@@ -0,0 +1,11 @@
+for(var i = 0; i < 5; i++) { 
+		console.log(i); 
+	}
+        
+        for(var i = 0; i < 5; i++) { 
+		setTimeout(function() { 
+			console.log(window.i);
+                        console.log(window.i === i );
+			console.log(i); 
+		}, 100); 
+	} 
